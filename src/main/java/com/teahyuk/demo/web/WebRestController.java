49d44966f8fd28a8021aeb88231b2c0149d23b44
teahyuk <jtaeh57@gmail.com>
2019-04-25 22:23:29 +0900

    jpa 추가
    게시판 entity 추가.
    BaseTime Entity 추가.
    jpa dependency 추가.

--- src/main/java/com/teahyuk/demo/web/WebRestController.java
+++ src/main/java/com/teahyuk/demo/web/WebRestController.java
@@ -1,12 +1,23 @@
 package com.teahyuk.demo.web;
 
+import com.teahyuk.demo.domain.dto.PostsSaveRequestDto;
+import com.teahyuk.demo.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+	private PostsRepository postsRepository;
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "HelloWorldff";
+	}
+
+	@PostMapping("/posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto){
+		postsRepository.save(dto.toEntity());
 	}
 }
