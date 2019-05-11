14a9948174b205b08803294e5c30303dba433f6d
teahyuk <jtaeh57@gmail.com>
2019-05-11 17:13:05 +0900

    add getProfile API

--- src/main/java/com/teahyuk/demo/web/WebRestController.java
+++ src/main/java/com/teahyuk/demo/web/WebRestController.java
@@ -3,13 +3,17 @@ package com.teahyuk.demo.web;
 import com.teahyuk.demo.domain.dto.PostsSaveRequestDto;
 import com.teahyuk.demo.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
 	private PostsService postsService;
+	private Environment env;
 
 	@GetMapping("/hello")
 	public String hello() {
@@ -19,5 +23,12 @@ public class WebRestController {
 	@PostMapping("/posts")
 	public void savePosts(@RequestBody PostsSaveRequestDto dto){
 		postsService.save(dto);
+	}
+
+	@GetMapping("/profile")
+	public String getProfile(){
+		return Arrays.stream(env.getActiveProfiles())
+				.findFirst()
+				.orElse("");
 	}
 }
