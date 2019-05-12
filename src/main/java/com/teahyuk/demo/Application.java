fa8cdce5e6341a1166d045f1fcdb39a83dd807b0
teahyuk <jtaeh57@gmail.com>
2019-05-12 13:31:12 +0900

    add run External props

--- src/main/java/com/teahyuk/demo/Application.java
+++ src/main/java/com/teahyuk/demo/Application.java
@@ -2,14 +2,20 @@ package com.teahyuk.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	private static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/SpringExam/real-application.yml";
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
 }
